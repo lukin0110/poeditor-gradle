a91fe96d10f7a426386ebbe45594d5664e8be4bd
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2015-02-26 16:59:24 +0100

    initial travis file

--- src/test/groovy/be/lukin/poeditor/gradle/PluginTest.groovy
+++ src/test/groovy/be/lukin/poeditor/gradle/PluginTest.groovy
@@ -1,7 +1,34 @@
 package be.lukin.poeditor.gradle
 
-/**
- * Created by maartenhuijsmans on 26/2/15.
- */
+import be.lukin.poeditor.gradle.tasks.InitTask
+import be.lukin.poeditor.gradle.tasks.PullTask
+import be.lukin.poeditor.gradle.tasks.PushTermsTask
+import org.junit.Test
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.api.Project
+import static org.junit.Assert.*
+
 class PluginTest {
+    
+    @Test
+    public void poeditorPluginAddsTasksToProject() {
+        
+        Project project = ProjectBuilder.builder().build()
+        project.apply plugin: 'poeditor'
+        
+        def container = project.tasks
+        assertEquals(3, container.size())
+        
+        // Random task
+        assertTrue(container.findByName('poeditor42') == null)
+        
+        assertTrue(container.findByName('poeditorInit') != null)
+        assertTrue(project.tasks.poeditorInit instanceof InitTask)
+        
+        assertTrue(container.findByName('poeditorPushTerms') != null)
+        assertTrue(project.tasks.poeditorPushTerms instanceof PushTermsTask)
+        
+        assertTrue(container.findByName('poeditorPull') != null)
+        assertTrue(project.tasks.poeditorPull instanceof PullTask)
+    }
 }
--- src/test/groovy/be/lukin/poeditor/gradle/TaskTest.groovy
+++ src/test/groovy/be/lukin/poeditor/gradle/TaskTest.groovy
@@ -1,7 +1,25 @@
 package be.lukin.poeditor.gradle
 
-/**
- * Created by maartenhuijsmans on 26/2/15.
- */
+import be.lukin.poeditor.gradle.tasks.InitTask
+import be.lukin.poeditor.gradle.tasks.PullTask
+import be.lukin.poeditor.gradle.tasks.PushTermsTask
+import org.junit.Test
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.api.Project
+import static org.junit.Assert.*
+
 class TaskTest {
+    @Test
+    public void canAddTaskToProject() {
+        Project project = ProjectBuilder.builder().build()
+        
+        def task1 = project.task('poeditorInit', type: InitTask)
+        assertTrue(task1 instanceof InitTask)
+        
+        def task2 = project.task('poeditorPushTerms', type: PushTermsTask)
+        assertTrue(task2 instanceof PushTermsTask)
+
+        def task3 = project.task('poeditorPull', type: PullTask)
+        assertTrue(task3 instanceof PullTask)
+    }
 }
